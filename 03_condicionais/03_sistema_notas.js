fdfb47e7bb2b5c640cefe7d4ae378a5766ae58bf
SayjinBlackBelt <116757037+sayjinblackbelt@users.noreply.github.com>
2026-08-29 21:17:46 -0300

    feat: add conditional logic exercises

---
+++ 03_condicionais/03_sistema_notas.js
@@ -0,0 +1,11 @@
+// 03 — Sistema simples de notas
+
+const media = 6.5;
+
+if (media >= 7) {
+  console.log("Aprovado.");
+} else if (media >= 5) {
+  console.log("Recuperação.");
+} else {
+  console.log("Reprovado.");
+}
