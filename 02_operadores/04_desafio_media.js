f5d1bc9dfff08f41ebac4837a11e1492dabb9f9f
SayjinBlackBelt <116757037+sayjinblackbelt@users.noreply.github.com>
2026-08-29 21:16:56 -0300

    feat: add operators exercises

---
+++ 02_operadores/04_desafio_media.js
@@ -0,0 +1,11 @@
+// Desafio — Média e aprovação
+
+const nota1 = 8;
+const nota2 = 7;
+const nota3 = 9;
+
+const media = (nota1 + nota2 + nota3) / 3;
+const aprovado = media >= 7;
+
+console.log("Média:", media.toFixed(2));
+console.log("Aprovado?", aprovado);
