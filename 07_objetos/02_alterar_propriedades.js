0384330906d11579ab57b5097593ec829b791819
SayjinBlackBelt <116757037+sayjinblackbelt@users.noreply.github.com>
2026-08-29 21:19:44 -0300

    feat: add objects exercises

---
+++ 07_objetos/02_alterar_propriedades.js
@@ -0,0 +1,12 @@
+// 02 — Acessando e alterando propriedades
+
+const computador = {
+  marca: "Lenovo",
+  memoriaGB: 24,
+  ligado: true
+};
+
+computador.memoriaGB = 32;
+computador.sistema = "Windows";
+
+console.log(computador);
