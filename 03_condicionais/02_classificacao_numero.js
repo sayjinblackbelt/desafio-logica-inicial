7f8e54a4b83d691988d9e084ea4483b4c903f197
SayjinBlackBelt <116757037+sayjinblackbelt@users.noreply.github.com>
2026-08-29 21:17:44 -0300

    feat: add conditional logic exercises

---
+++ 03_condicionais/02_classificacao_numero.js
@@ -0,0 +1,11 @@
+// 02 — Classificação de número
+
+const numero = -5;
+
+if (numero > 0) {
+  console.log("Número positivo.");
+} else if (numero < 0) {
+  console.log("Número negativo.");
+} else {
+  console.log("O número é zero.");
+}
