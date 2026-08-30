f4bbcbeb3433e09120e668c5ee764440411c0ec1
SayjinBlackBelt <116757037+sayjinblackbelt@users.noreply.github.com>
2026-08-29 21:18:14 -0300

    feat: add repetition exercises

---
+++ 04_repeticao/02_tabuada.js
@@ -0,0 +1,7 @@
+// 02 — Tabuada
+
+const numero = 7;
+
+for (let multiplicador = 1; multiplicador <= 10; multiplicador++) {
+  console.log(`${numero} x ${multiplicador} = ${numero * multiplicador}`);
+}
