8b2806c370048078d64a7ad98ae07fcc4854cb35
SayjinBlackBelt <116757037+sayjinblackbelt@users.noreply.github.com>
2026-08-29 21:16:20 -0300

    feat: add introductory variables exercises

---
+++ 01_variaveis/03_operacoes_basicas.js
@@ -0,0 +1,14 @@
+// 03 — Operações básicas
+
+const numeroA = 10;
+const numeroB = 5;
+
+const soma = numeroA + numeroB;
+const subtracao = numeroA - numeroB;
+const multiplicacao = numeroA * numeroB;
+const divisao = numeroA / numeroB;
+
+console.log("Soma:", soma);
+console.log("Subtração:", subtracao);
+console.log("Multiplicação:", multiplicacao);
+console.log("Divisão:", divisao);
