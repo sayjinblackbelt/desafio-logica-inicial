d83df290b4906f3d8e9da4f7451d194ba9433163
SayjinBlackBelt <116757037+sayjinblackbelt@users.noreply.github.com>
2026-08-29 21:16:49 -0300

    feat: add operators exercises

---
+++ 02_operadores/01_aritmeticos.js
@@ -0,0 +1,11 @@
+// 01 — Operadores aritméticos
+
+const a = 12;
+const b = 4;
+
+console.log("Soma:", a + b);
+console.log("Subtração:", a - b);
+console.log("Multiplicação:", a * b);
+console.log("Divisão:", a / b);
+console.log("Resto:", a % b);
+console.log("Potência:", a ** b);
