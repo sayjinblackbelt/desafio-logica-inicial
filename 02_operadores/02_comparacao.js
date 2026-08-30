d4e7e2b562acb40468d4e74120987e428430a883
SayjinBlackBelt <116757037+sayjinblackbelt@users.noreply.github.com>
2026-08-29 21:16:52 -0300

    feat: add operators exercises

---
+++ 02_operadores/02_comparacao.js
@@ -0,0 +1,9 @@
+// 02 — Operadores de comparação
+
+const idade = 18;
+const idadeMinima = 18;
+
+console.log("Igualdade estrita:", idade === idadeMinima);
+console.log("Maior que:", idade > idadeMinima);
+console.log("Maior ou igual:", idade >= idadeMinima);
+console.log("Menor que:", idade < idadeMinima);
