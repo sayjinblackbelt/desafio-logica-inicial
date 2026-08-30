234ed133b75f68cfe75e9e69181b19bf17209afd
SayjinBlackBelt <116757037+sayjinblackbelt@users.noreply.github.com>
2026-08-29 21:19:11 -0300

    feat: add arrays exercises

---
+++ 06_arrays/01_criar_array.js
@@ -0,0 +1,7 @@
+// 01 — Criando e acessando arrays
+
+const linguagens = ["JavaScript", "Python", "HTML", "CSS"];
+
+console.log("Lista:", linguagens);
+console.log("Primeira linguagem:", linguagens[0]);
+console.log("Quantidade:", linguagens.length);
