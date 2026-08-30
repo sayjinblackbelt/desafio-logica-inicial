c3d29c68f9f69ed7a26291b3c63b67fad7ab4fa0
SayjinBlackBelt <116757037+sayjinblackbelt@users.noreply.github.com>
2026-08-29 21:16:18 -0300

    feat: add introductory variables exercises

---
+++ 01_variaveis/02_tipos_de_dados.js
@@ -0,0 +1,11 @@
+// 02 — Tipos de dados
+
+const texto = "Olá, mundo!";
+const numero = 44;
+const decimal = 1.76;
+const ativo = true;
+
+console.log(typeof texto);
+console.log(typeof numero);
+console.log(typeof decimal);
+console.log(typeof ativo);
